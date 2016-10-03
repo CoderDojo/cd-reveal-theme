9f635cbc57ebc1a3635fb4e2d4a8a64280f1084c
Daniel Brierton <daniel@coderdojo.org>
2016-10-03 16:00:57 +0100

    Fix font not being added to dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('copy-images', function () {
-  return gulp.src('./images/*')
+  return gulp.src('./images/**/*')
     .pipe(gulp.dest('./dist/images/'));
+}); 
+
+gulp.task('copy-fonts', function () {
+  return gulp.src('./font/**/*')
+    .pipe(gulp.dest('./dist/font/'));
 }); 
 
 gulp.task('sass', function () {
@@ -12,4 +17,4 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['copy-images', 'sass']);
+gulp.task('default', ['copy-images', 'copy-fonts', 'sass']);
